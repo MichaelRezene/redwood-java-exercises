ed6508e5f2121c49eb4dd165746fd00924c9ef55
michael <mgebregziabher86@gmail.com>
2017-09-18 15:21:54 -0500

    Solution for the first exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,80 @@
 public class HelloWorld {
     // psvm enter could direct you
     public static void main(String[] args){
-        //sout will direct you
-        System.out.print("Hello World!");
+
+
+        int myFavoriteNumber = 4;
+        System.out.println("My favorite number is " + myFavoriteNumber + ".");
+
+        String myString = "Micky";
+        char myChar = 'G';
+        System.out.println("My favorite name is " + myString + ".");
+        System.out.println("My favorite Char is " + myChar + ".");
+
+        double doubleNumber = 3.14;
+        System.out.println("My double number is " + doubleNumber + ".");
+
+        long myNumber = 123L;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+
+        /* floating point number
+         //sout will direct you
+        // general syntax: type identifier;
+        // 3 types of integer:
+        short shortNumber = 0;
+        long longNumber = 0;
+        int  intNumber = 0;
+
+        float floatNumber = 2.0f; // if you forget the f the code doesn't compile
+        double doubleNumber = 10.5;
+        //
+        char singleCharacter = 'l'; // always single quotes
+        //
+        boolean booleanValue = true;
+        //
+        String name = ("Redwood");  //always double quotes
+
+        // const MY_CONSTANT = 4;
+        final int MY_CONSTANT = 4;
+
+        System.out.println(3 + 4);
+        System.out.println(3 - 4);
+        System.out.println(3 / 4);
+        System.out.println(3 * 4);
+
+        float division = 3.0f / 4;
+        System.out.println(division);
+
+        System.out.println(3 % 4);
+
+        int x = 5;
+        System.out.println(x++); // 5
+        System.out.println(++x); // 7
+        System.out.println(x--); // 7
+        System.out.println(--x); // 5
+
+
+        x += 3; // 8
+        System.out.println(x);
+        x %= 2; //
+        System.out.println(x);
+
+         // It is only valid if the value on the right is smaller
+         //than the type in the left
+        double aDoubleNumber = 10; // from int to double
+        System.out.println(aDoubleNumber);
+*/
+        System.out.println("Hello World!");
+        System.out.println("Hello Redwood!");
 
     }
 }
