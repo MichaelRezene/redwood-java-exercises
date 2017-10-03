7cec3f080a49478aa874401ed2f31c8f41714204
michael <mgebregziabher86@gmail.com>
2017-10-03 15:52:03 -0500

    complete solution for the Exception Exercise  -getInt()  -getDouble()

--- src/util/Input.java
+++ src/util/Input.java
@@ -21,38 +21,37 @@ package util;
                 return userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes");
             }
 
-            public int getInt() {
-                if (this.scanner.hasNextInt()) {
-                        return this.scanner.nextInt();
-                    } else {
-                        System.out.println("That's not an integer! Try again.");
-                        scanner.nextLine();
-                        return getInt();
-                    }
 
-                   }
+            public int getInt(){
+                try {
+                   return Integer.valueOf(getString("Enter number here: "));
+
+                } catch (NumberFormatException e) {
+
+                    System.out.println("That's not an integer! Try again.");
+                    scanner.nextLine();
+                    return getInt();
+                }
+            }
 
              public int getInt(int min, int max) {
                 int userInput = getInt();
-
                         if (userInput < min || userInput > max) {
                      System.out.println(userInput + " is not between " + min + " and " + max);
                         return getInt(min, max);
                     }
-
                        return userInput;
         }
-
              public double getDouble() {
-                if (this.scanner.hasNextDouble()) {
-                      return this.scanner.nextDouble();
-                    } else {
-                        System.out.println("That's not an double! Try again.");
+                try {
+                    return Double.valueOf(getString("Enter Decimal number here: "));
+
+                }catch (NumberFormatException ex){
+                        System.out.println("That's not an decimal number! try again.");
                         scanner.nextLine();
                         return getDouble();
                     }
          }
-
              public double getDouble(double min, double max) {
                 double userInput = getDouble();
 
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -5,9 +5,14 @@ import util.Input;
         public class InputTest {
     public static void main(String[] args) {
                Input input = new Input();
-                       System.out.print("Enter a number between 1 and 10");
-               int number = input.getInt(1, 10);
-
+            System.out.print("Enter a number between 1 and 10");
+               int number = input.getInt();
                         System.out.println("You enetered: " + number);
+
+        double anOtherNum = input.getDouble();
+        System.out.println("Your decimal number is: " + anOtherNum);
+
+        System.out.println("We almost finish.");
+
           }
  }
